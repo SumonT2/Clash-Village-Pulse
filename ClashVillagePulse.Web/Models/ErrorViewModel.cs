7f70b0b24650e69885c6be53d8fd64d2bcae5436
SumonT2 <sumon@techsflow.com>
2026-03-04 21:59:15 +0600

    Create MVC Web project

---
+++ ClashVillagePulse.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClashVillagePulse.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
